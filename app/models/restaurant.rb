d983c16154f919c356e29d3b51fe996d787b003b
Holly McMahon <hmcmahon25@gmail.com>
2020-05-20 22:47:04 +0200

    routing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates_presence_of :name, :address, :category
-  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   validates :category, inclusion: { in: CATEGORIES }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "restaurants", to: "restaurants#index"
+  get "restaurants/:id", to: "restaurants#show"
   get "restaurants/new", to: "restaurants#new"
 end
